46727eda2a3e19eed861a65082b3719026167b10
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-13 19:31:20 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artists.uniq
+    artists.uniq.collect do |e|
+      e.name
+    end
   end
 end
